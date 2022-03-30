b998556103d7d3648ea598d9f9f6ee7363068eb5
belmapeskic <belma.peskic@gmail.com>
2022-03-31 01:41:59 +0200

    Adding file index.js

---
+++ ex05/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below line
+    var x;
+    var x = 8;
+    // Only change code above line
+    return x;
+
+}
+
+console.log(main());
+
+module.exports = main;
