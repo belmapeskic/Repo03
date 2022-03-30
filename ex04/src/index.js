5b5fdcb5b9cd77d258bd99d135cec90bbb761e5d
belmapeskic <belma.peskic@gmail.com>
2022-03-31 01:15:15 +0200

    Adding file index.js

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // Only change code below this line
+    var x;
+    var y;
+    x = 10;
+    y = x;
+    // only change code above this line
+    return [x, y];
+
+}
+
+console.log(main());
+
+module.exports = main;
