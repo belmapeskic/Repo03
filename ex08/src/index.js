3b0caa2528914ac8175421b620b885bf6aca1a05
belmapeskic <belma.peskic@gmail.com>
2022-03-31 06:42:54 +0200

    Adding file index.js

---
+++ ex08/src/index.js
@@ -0,0 +1,15 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    var sum;
+    var numOne;
+    var numTwo;
+    sum = numOne + numTwo;
+    // Only change code above this line
+    return sum;
+}
+
+console.log(main(15, 8)); // assigned 23
+console.log(main(5, 28)); // assigned 33
+console.log(main(150, 425)); // assigned 575
+
+module.exports = main;
