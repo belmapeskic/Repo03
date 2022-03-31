0174c20cdbd8eb38ff8f717d1158f26fbed54a35
belmapeskic <belma.peskic@gmail.com>
2022-03-31 03:02:21 +0200

    Adding file index.js

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -8,6 +8,7 @@ function main() {
     studlyCapVar = 10;
     properCamelCase = "A String";
     titleCaseOver = 9000;
+    // Only change code above this line
     return {
         studlyCapVar,
         properCamelCase,
